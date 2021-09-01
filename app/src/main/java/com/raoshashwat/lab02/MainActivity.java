6a826eb41234242aaea8820cd6e040f9b3678b5d
Shash_Rao <shashwat.a.rao@gmail.com>
2021-09-01 13:50:02 -0400

    final without embellishments

--- app/src/main/java/com/raoshashwat/lab02/MainActivity.java
+++ app/src/main/java/com/raoshashwat/lab02/MainActivity.java
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.SpannableStringBuilder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
-    TextView display;
+    TextView nameDisplay;
+    TextView cycleDisplay;
     String name;
     EditText enter;
     String[] arguments;
@@ -23,26 +31,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.submit_button);
-        display = findViewById(R.id.name_display);
+        nameDisplay = findViewById(R.id.name_display);
+        cycleDisplay = findViewById(R.id.cycle_display);
         enter = findViewById(R.id.name_enter);
         arguments = getResources().getStringArray(R.array.args);
         name = "";
-        display.setText(arguments[count]);
+        cycleDisplay.setText(arguments[count]);
     }
 
     public void submit(View view) {
         name = String.valueOf(enter.getText());
-        
+        nameDisplay.setText(name);
     }
 
     public void increment(View view) {
         if(++count >= arguments.length) count = 0;
-        display.setText(arguments[count % arguments.length]);
+        cycleDisplay.setText(arguments[count % arguments.length]);
     }
 
 
     public void decrement(View view) {
         if(--count < 0) count = arguments.length - 1;
-        display.setText(arguments[count % arguments.length]);
+        cycleDisplay.setText(arguments[count % arguments.length]);
     }
 }
