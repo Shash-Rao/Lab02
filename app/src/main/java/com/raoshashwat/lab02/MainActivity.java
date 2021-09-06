ea48c76f18b783d64e3eabb8df69cde2658864c0
Shash-Rao <shashwat.a.rao@gmail.com>
2021-09-06 17:25:12 -0400

    final with embellishments

--- app/src/main/java/com/raoshashwat/lab02/MainActivity.java
+++ app/src/main/java/com/raoshashwat/lab02/MainActivity.java
@@ -2,6 +2,9 @@ package com.raoshashwat.lab02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.SpannableStringBuilder;
@@ -9,18 +12,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
     TextView nameDisplay;
     TextView cycleDisplay;
+    ImageView imageDisplay;
     String name;
     EditText enter;
     String[] arguments;
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.submit_button);
         nameDisplay = findViewById(R.id.name_display);
         cycleDisplay = findViewById(R.id.cycle_display);
+        imageDisplay = findViewById(R.id.image_display);
         enter = findViewById(R.id.name_enter);
         arguments = getResources().getStringArray(R.array.args);
         name = "";
@@ -44,14 +51,21 @@ public class MainActivity extends AppCompatActivity {
         nameDisplay.setText(name);
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     public void increment(View view) {
         if(++count >= arguments.length) count = 0;
         cycleDisplay.setText(arguments[count % arguments.length]);
+        String drawableName = arguments[count].toLowerCase();
+        int resId = getResources().getIdentifier(drawableName , "drawable", getPackageName());
+        imageDisplay.setImageResource(resId);
     }
 
 
     public void decrement(View view) {
         if(--count < 0) count = arguments.length - 1;
         cycleDisplay.setText(arguments[count % arguments.length]);
+        String drawableName = arguments[count].toLowerCase();
+        int resId = getResources().getIdentifier(drawableName , "drawable", getPackageName());
+        imageDisplay.setImageResource(resId);
     }
 }
